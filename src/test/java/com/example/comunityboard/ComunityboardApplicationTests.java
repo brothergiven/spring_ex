a44bdae884aff6fd6ebae8a51836110fc0ab3d87
brothergiven <dung3740@gmail.com>
2025-02-25 18:47:04 +0900

    chore: Initial commit

---
+++ src/test/java/com/example/comunityboard/ComunityboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comunityboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunityboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
